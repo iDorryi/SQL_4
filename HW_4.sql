b2176ab00dd713f0e285f1cc63e5fedb8d7e7c8c
idorry <iannoyinbox@gmail.com>
2023-04-30 00:43:33 +0300

    asddsd

--- HW_4.sql
+++ HW_4.sql
@@ -4,11 +4,16 @@ FROM auto
 WHERE mark IN("BMW", "LADA")
 GROUP BY color,mark;
 #2
-SELECT DISTINcT mark, (select count(1)
-FROM auto a1
-WHERE a1.mark != a.mark) AS count
-FROM auto a;
-CREATE TABLE test_a
+SELECT mc.mark,
+     mc.mark_count,
+       (au.count_auto - mc.mark_count) AS 'другие авто'
+FROM (SELECT mark,
+         COUNT(*) AS mark_count
+    FROM auto
+      GROUP BY mark
+) AS mc
+CROSS JOIN (SELECT COUNT(*) AS count_auto
+      FROM auto) AS au;
 #3
 SELECT * 
 FROM test_a
